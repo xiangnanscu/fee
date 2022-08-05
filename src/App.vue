e892bc24ea5c766b13e8091de1708c1ff835287b
xn <280145668@qq.com>
2022-08-05 11:11:24 +0800

    push

--- src/App.vue
+++ src/App.vue
@@ -21,13 +21,13 @@ const FeeModel = Model.makeClass({
   },
 });
 
-const gwy = '王廷江,董茂明,项楠,肖宇,蒲洁,魏涛,龙曦'.split(',');
-const sy = '杨洁,陈乾,袁良会'.split(',');
+const gwy = '王雪莲,王廷江,董文茹,董茂明,项楠,肖宇,蒲洁,魏涛,龙曦,谭港,李意,朱智琦,刘洪,邹宏宇,陈志锐,范敏,唐一铭,李重安,贺鹤泉'.split(',');
+const sy = '景明江,杨洁,陈乾,袁良会,熊丽萍,吴开来,罗忠友,曾占梅,雷兰,刘俐,王珏'.split(',');
 const users = {
   ...Object.fromEntries(gwy.map(e => [e.trim(), 'g'])),
   ...Object.fromEntries(sy.map(e => [e.trim(), 's']))
 };
-
+console.log(Object.keys(users).length)
 function s2ab(s) {
   var buf = new ArrayBuffer(s.length);
   var view = new Uint8Array(buf);
@@ -58,15 +58,17 @@ function templateToFile({ template, filename, table }) {
   );
 }
 const makeItem = () => ({
-  start_year: "",
-  start_month: "",
-  start_day: "",
+  // start_year: "",
+  // start_month: "",
+  // start_day: "",
+  start_date: "",
   start_address: "",
-  end_year: "",
-  end_month: "",
-  end_day: "",
+  // end_year: "",
+  // end_month: "",
+  // end_day: "",
+  end_date: "",
   end_address: "",
-  days: 0,
+  // days: 0,
   reason: "",
   bzf_bz: 0,
   bzf_je: 0,
@@ -83,7 +85,10 @@ const makeItem = () => ({
   i: "",
 });
 
-const inputValue = ref(`张三,李四|2022|7|3|江安|2022|7|3|宜宾|1|公招体检`);
+const inputValue = ref(`项楠|2022.6.3|江安|2022.6.3|宜宾|报送资料
+项楠|2022.6.10|江安|2022.6.10|阳春|报送资料
+董茂明,杨洁,项楠|2022.7.4|江安|2022.7.5|宜宾|事业公招体检第二批
+项楠|2022.5.3|江安|2022.5.3|成都|公招考察`);
 
 const getFeeStandard = (address, flag) => {
   if (/^宜宾市?$/.test(address)) {
@@ -92,27 +97,37 @@ const getFeeStandard = (address, flag) => {
     return flag == 'g' ? { bzf_bz: 80, zf_bz: 20 } : { bzf_bz: 80, zf_bz: 40 };
   } else if (/^(江安|下长|阳春|铁清|四面山|怡乐|留耕|夕佳山|大妙|仁和|大井|红桥|五矿|迎安)镇?$/.test(address)) {
     return { bzf_bz: 50, zf_bz: 0 };
-  } else if (/^.+市?$/.test(address)) {
+  } else if (/^(成都|泸州|广元|遂宁|南充|达州|攀枝花|眉山|广安|雅安|巴中|内江|乐山|资阳|自贡|德阳|绵阳)市?$/.test(address)) {
     return flag == 'g' ? { bzf_bz: 100, zf_bz: 25 } : { bzf_bz: 100, zf_bz: 50 };
   } else {
-    throw `无法解析的地址${address}`;
+    throw `无法识别地址"${address}",规则举例: 1. 区县: 翠屏区, 长宁, 三江新区; 2. 乡镇: 阳春, 四面山; 3.市: 宜宾, 成都`;
   }
 };
+const getDate = (s) => {
+  const d = new Date(s)
+  if (!(d instanceof Date && !isNaN(d.valueOf()))) {
+    throw `日期不正确:${s}`
+  }
+  return d
+}
+const ONE_DAY = 1000 * 60 * 60 * 24
 const parseItem = line => {
   const [nameString, ...ia] = line.split('|');
   const rawItem = makeItem();
   const item = Object.fromEntries(Object.keys(rawItem).map((e, i) => [e, ia[i] || rawItem[e]]));
-  const names = nameString.split(/[,]/);
-  const days = Number(item.days);
-  const end_address = item.end_address;
+  const names = nameString.split(/[,、，]/);
+  const sd = getDate(item.start_date)
+  const ed = getDate(item.end_date)
+  const days = Math.abs((sd - ed) / ONE_DAY) + 1;
+  const target_address = /江安县?/.test(item.end_address) ? item.start_address : item.end_address;
   const bzfSet = new Set();
   const zfSet = new Set();
   for (const name of names) {
     let flag = users[name];
     if (!flag) {
-      throw `找不到职工${name}`;
+      throw `找不到职工"${name}"`;
     }
-    const { bzf_bz, zf_bz } = getFeeStandard(end_address, flag);
+    const { bzf_bz, zf_bz } = getFeeStandard(target_address, flag);
     item.bzf_je += bzf_bz * days;
     item.zf_je += zf_bz * days;
     bzfSet.add(bzf_bz);
@@ -122,10 +137,14 @@ const parseItem = line => {
   item.zf_bz = Array.from(zfSet).sort().join('/');
   item.total = item.bzf_je + item.zf_je;
   item.names = names;
-  item.start_date = `${item.start_year}.${item.start_month}.${item.start_day}`;
-  item.end_date = item.start_year === item.end_year ? `${item.end_month}.${item.end_day}` : `${item.end_year}.${item.end_month}.${item.end_day}`;
+  // item.start_date = `${item.start_year}.${item.start_month}.${item.start_day}`;
+  // item.end_date = item.start_year === item.end_year ? `${item.end_month}.${item.end_day}` : `${item.end_year}.${item.end_month}.${item.end_day}`;
+  if (sd.getFullYear() == ed.getFullYear()) {
+    item.end_date = `${ed.getMonth() + 1}.${ed.getDate()}`
+  }
   item.date = `${item.start_date}—${item.end_date}`;
   item.address = `${item.start_address}—${item.end_address}`;
+  item.days = days
   return item;
 };
 
@@ -140,10 +159,14 @@ const getCnNumberList = (acc) => {
   }
   return cn;
 };
-async function download(event) {
+function download(event) {
   const items = [];
   for (const line of inputValue.value.split('\n')) {
-    const item = parseItem(line);
+    const cline = line.replaceAll(/\s/g, "")
+    if (!cline) {
+      continue
+    }
+    const item = parseItem(cline);
     items.push(item);
   }
   const names = items.flatMap(e => e.names).filter((e, i, a) => a.indexOf(e) === i).join(',');
@@ -153,14 +176,18 @@ async function download(event) {
     items.push({});
   }
   if (total > 9999) {
-    throw "总金额不能大于9999元";
+    throw `总金额${total}已大于9999元,请检查出差总天数${items.filter(e => e.days).map(e => e.days).reduce((x, y) => x + y)}是否正确`;
   }
   const cn = getCnNumberList(total);
+  const now = new Date()
   templateToFile({
     template: feeTemplate,
     filename: "差旅费报销明细表",
     table: {
       names,
+      year: now.getFullYear(),
+      month: now.getMonth() + 1,
+      day: now.getDate(),
       qian: cn[0],
       bai: cn[1],
       shi: cn[2],
@@ -172,13 +199,25 @@ async function download(event) {
     },
   });
 }
+const errorMsg = ref("")
+const tryDownload = () => {
+  try {
+    errorMsg.value = ""
+    download()
+    console.log("error  not happens")
+  } catch (error) {
+    console.log("error happens")
+    errorMsg.value = error.message || error
+  }
+}
 </script>
 
 <template>
   <div style="width:980px;margin: auto;">
-    <span>姓名1,姓名2|2022|7|3|江安|2022|7|3|宜宾|1|公招体检</span>
-    <textarea v-model="inputValue" rows="20" class="block"></textarea>
-    <button class="block" @click="download">下载</button>
+    <span v-if="errorMsg" style=" color:red">{{ errorMsg }}</span>
+    <span v-else>仅适用于省内公车出差, 格式: <br />姓名|出发日期|出发地点|抵达日期|抵达地点|出差事由</span>
+    <textarea v-model="inputValue" rows="10" class="block" style="font-size:130%"></textarea>
+    <button class="block" @click="tryDownload">下载</button>
   </div>
 </template>
 
